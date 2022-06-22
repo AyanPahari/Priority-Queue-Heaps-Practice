e71ff1d0cb25ad9ad3e962d534f727689b17a36b
Ayan Kumar Pahari <pahari.ayan@gmail.com>
2022-06-22 19:27:02 +0530

    Time: 19 ms (24.65%), Space: 11.4 MB (39.42%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -3,8 +3,8 @@ typedef pair<int,int> pi;
 class Compare{
     public:
         bool operator()(pair<int,int> a, pair<int,int> b){
-            if(a.first == b.first) return a.second < b.second;
-            return a.first > b.first;
+            if(a.second == b.second) return a.first < b.first;
+            return a.second > b.second;
         }
 };
 
@@ -14,12 +14,12 @@ public:
         unordered_map<int,int>mp;
         for(auto it:nums) mp[it]++;
         priority_queue<pi, vector<pi>, Compare>minh;
-        for(auto it : mp) minh.push({it.second, it.first});
+        for(auto it : mp) minh.push({it.first, it.second});
         vector<int>res;
         
         while(!minh.empty()){
-            int count = minh.top().first;
-            int val = minh.top().second;
+            int count = minh.top().second;
+            int val = minh.top().first;
             while(count--) res.push_back(val);
             minh.pop();
         }
