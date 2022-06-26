fb1bb7313c7e71939ceec0f13344b0c44e2e3236
Ayan Kumar Pahari <pahari.ayan@gmail.com>
2022-06-27 01:01:42 +0530

    Added solution - LeetHub

---
+++ Rearrange characters - GFG/rearrange-characters.cpp
@@ -0,0 +1,90 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+const int MAX_CHAR = 26;
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    string rearrangeString(string s){
+        string str;
+        unordered_map<char,int>mp;
+        priority_queue<pair<int,char>>maxh;
+        for(int i=0;i<s.length();i++) mp[s[i]]++;
+        for(auto it: mp) maxh.push({it.second, it.first});
+        
+        pair<int,char> block = maxh.top();
+        maxh.pop();
+        str.push_back(block.second);
+        block.first = block.first - 1;
+        while(!maxh.empty()){
+            pair<int,char> temp = maxh.top();
+            maxh.pop();
+            str.push_back(temp.second);
+            temp.first = temp.first - 1;
+            if(block.first > 0) maxh.push(block);
+            block = temp;
+        }
+        if(block.first > 0) return "-1";
+        else return str;
+    }
+    
+};
+
+// { Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    string str;
+	    cin>>str;
+	    Solution ob;
+	    string str1=ob.rearrangeString(str);
+	    int flag=1;
+	    int c[26] = {0};
+	    for(int i=0; i<str.length(); i++)
+	        c[str[i]-'a']+=1;
+	    int f = 0;
+	    int x = (str.length()+1)/2;
+	    for(int i=0; i<26; i++)
+	    {
+	        if(c[i]>x)
+	            f = 1;
+	    }
+	    if(f)
+	    {
+	        if(str1=="-1")
+	            cout<<0<<endl;
+	        else
+	            cout<<1<<endl;
+	    }
+	    else
+	    {
+    	    int a[26] = {0};
+    	    int b[26] = {0};
+    	    for(int i=0; i<str.length(); i++)
+    	        a[str[i]-'a']+=1;
+    	    for(int i=0; i<str1.length(); i++)
+    	        b[str1[i]-'a']+=1;
+    	        
+    	    for(int i=0; i<26; i++)
+    	        if(a[i]!=b[i])
+    	            flag = 0;
+    	    
+    	    for(int i=0;i<str1.length();i++)
+    	    {
+    		    if(i>0)
+    		        if(str1[i-1]==str1[i])
+    			        flag=0;
+    	    }
+    	    if(flag==1)
+    		    cout<<"1\n";
+    	    else
+    	        cout<<"0\n";
+	    }
+	}
+    return 0;
+}  // } Driver Code Ends
