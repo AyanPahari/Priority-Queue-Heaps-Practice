f62dcd8118c98d383a6eeb46f74db2d4e81791a3
Ayan Kumar Pahari <pahari.ayan@gmail.com>
2022-06-28 06:56:23 +0530

    Added solution - LeetHub

---
+++ Distinct Substrings - GFG/distinct-substrings.cpp
@@ -0,0 +1,26 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+int fun(string s);
+
+int main() {
+    int tt;
+    string s;
+    cin >> tt;
+    while (tt--) {
+        cin >> s;
+        cout<<fun(s)<<endl;
+        
+    }
+    return 0;
+}// } Driver Code Ends
+
+
+int fun(string s)
+{
+    unordered_set<string>st;
+    for(int i=0;i<s.length()-1;i++){
+        st.insert(s.substr(i,2));
+    }
+    return st.size();
+}
