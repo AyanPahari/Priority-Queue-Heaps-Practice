3059717c2c4e89dd51828d40e9ca217274aa367d
Ayan Kumar Pahari <pahari.ayan@gmail.com>
2022-07-03 13:15:49 +0530

    Added solution - LeetHub

---
+++ Smallest window containing 0, 1 and 2 - GFG/smallest-window-containing-0-1-and-2.cpp
@@ -0,0 +1,53 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int smallestSubstring(string S) {
+        int i = 0, j = 0;
+        int count = 3, mini = INT_MAX;
+        unordered_map<char, int>mp;
+        mp['0']++;
+        mp['1']++;
+        mp['2']++;
+        
+        while(j < S.length()){
+            if(mp.find(S[j]) != mp.end()){
+                mp[S[j]]--;
+                if(mp[S[j]] == 0) count--;
+            }
+            if(count == 0){
+                while(count == 0){
+                    mini = min(mini, j - i + 1);
+                    if(mp.find(S[i]) != mp.end()){
+                        mp[S[i]]++;
+                        if(mp[S[i]] == 1) count++;
+                    }
+                    i++;
+                }
+            }
+            j++;
+        }
+        return mini == INT_MAX ? -1 : mini;
+    }
+};
+
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S;
+        cin >> S;
+        Solution ob;
+        cout << ob.smallestSubstring(S);
+        cout << endl;
+    }
+}  // } Driver Code Ends
